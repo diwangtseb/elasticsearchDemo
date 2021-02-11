e2f4dd2b5d60c00a1281ac7c5298da287770f520
wangdi <diwang839639311@163.com>
2021-02-11 15:47:00 +0800

    更新删除requirements文件，并且删除多余的快捷方式

--- dev/main.py
+++ dev/main.py
@@ -1,15 +1,17 @@
 # -*- coding:utf-8 -*-
-from flask import Flask, jsonify, request
-from savedata import db_action
-from elasticsearchdata import add_to_esdata
-from flask_cors import CORS, cross_origin
 import os
+
+from flask import Flask, jsonify, request
+# from elasticsearchdata import add_to_esdata
+from flask_cors import CORS
 from flask_request_params import bind_request_params
+
+from savedata import db_action
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 app.before_request(bind_request_params)
-cors = CORS(app) # 跨域
+cors = CORS(app)  # 跨域
 
 music_db = db_action.SqliteDatabase(f'{os.getcwd()}\\savedata\\music.db')
 
@@ -20,21 +22,21 @@ def homepage():
     return jsonify(res)
 
 
-@app.route('/search_sings')
-def search_sings():
-    # 获取request中的查询参数
-    name = request.values.get('Name')
-    query = {
-        "query": {
-            "match_phrase_prefix": {
-                "Name": f"{name}"
-            }
-        }
-    }
-
-    music_info = add_to_esdata.es.search(index="music", doc_type="musicInfo", body=query,
-                                         filter_path=["hits.hits._source"])
-    return jsonify(music_info)
+# @app.route('/search_sings')
+# def search_sings():
+#     # 获取request中的查询参数
+#     name = request.values.get('Name')
+#     query = {
+#         "query": {
+#             "match_phrase_prefix": {
+#                 "Name": f"{name}"
+#             }
+#         }
+#     }
+#
+#     music_info = add_to_esdata.es.search(index="music", doc_type="musicInfo", body=query,
+#                                          filter_path=["hits.hits._source"])
+#     return jsonify(music_info)
 
 
 @app.route('/user', methods=['POST'])
@@ -44,4 +46,4 @@ def create_user():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",debug=True)
+    app.run(host="0.0.0.0", debug=True)
